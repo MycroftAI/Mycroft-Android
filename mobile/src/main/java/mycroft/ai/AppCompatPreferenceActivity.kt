bd2eade9e8d50dc9bb5bf81717d7dea6970bf440
sarah <srkraynick@gmail.com>
2018-07-25 16:36:09 -0600

    Ported AppComPreferenceAcitivity.

---
+++ mobile/src/main/java/mycroft/ai/AppCompatPreferenceActivity.kt
@@ -0,0 +1,120 @@
+/*
+ *  Copyright (c) 2017. Mycroft AI, Inc.
+ *
+ *  This file is part of Mycroft-Android a client for Mycroft Core.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package mycroft.ai
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.preference.PreferenceActivity
+import android.support.annotation.LayoutRes
+import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatDelegate
+import android.support.v7.widget.Toolbar
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A [android.preference.PreferenceActivity] which implements and proxies the necessary calls
+ * to be used with AppCompat.
+ */
+abstract class AppCompatPreferenceActivity : PreferenceActivity() {
+
+    private lateinit var mDelegate: AppCompatDelegate
+
+    val supportActionBar: ActionBar?
+        get() = delegate.supportActionBar
+
+    private val delegate: AppCompatDelegate
+        get() {
+            if (::mDelegate.isInitialized) {
+                return mDelegate;
+
+            } else {
+                mDelegate = AppCompatDelegate.create(this, null)
+
+                return mDelegate
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        delegate.installViewFactory()
+        delegate.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        delegate.onPostCreate(savedInstanceState)
+    }
+
+    fun setSupportActionBar(toolbar: Toolbar?) {
+        delegate.setSupportActionBar(toolbar)
+    }
+
+    override fun getMenuInflater(): MenuInflater {
+        return delegate.menuInflater
+    }
+
+    override fun setContentView(@LayoutRes layoutResID: Int) {
+        delegate.setContentView(layoutResID)
+    }
+
+    override fun setContentView(view: View) {
+        delegate.setContentView(view)
+    }
+
+    override fun setContentView(view: View, params: ViewGroup.LayoutParams) {
+        delegate.setContentView(view, params)
+    }
+
+    override fun addContentView(view: View, params: ViewGroup.LayoutParams) {
+        delegate.addContentView(view, params)
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        delegate.onPostResume()
+    }
+
+    override fun onTitleChanged(title: CharSequence, color: Int) {
+        super.onTitleChanged(title, color)
+        delegate.setTitle(title)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        delegate.onConfigurationChanged(newConfig)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        delegate.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        delegate.onDestroy()
+    }
+
+    override fun invalidateOptionsMenu() {
+        delegate.invalidateOptionsMenu()
+    }
+}
